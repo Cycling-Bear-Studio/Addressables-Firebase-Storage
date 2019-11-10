3161ab515978c6f3e77a0d5bd9a901d0b08b12a6
Robin Fischer <t9RCtCxQZFp@gmail.com>
2019-11-10 10:08:42 +0100

    Make sure dependant AssetBundle handle is released

--- Storage/Addressables/FirebaseStorageAssetBundleProvider.cs
+++ Storage/Addressables/FirebaseStorageAssetBundleProvider.cs
@@ -5,7 +5,9 @@ namespace RobinBird.FirebaseTools.Storage.Addressables
     using Firebase.Extensions;
     using Firebase.Storage;
     using UnityEngine;
+    using UnityEngine.AddressableAssets;
     using UnityEngine.AddressableAssets.ResourceLocators;
+    using UnityEngine.ResourceManagement.AsyncOperations;
     using UnityEngine.ResourceManagement.ResourceLocations;
     using UnityEngine.ResourceManagement.ResourceProviders;
 
@@ -15,6 +17,23 @@ namespace RobinBird.FirebaseTools.Storage.Addressables
     [DisplayName("Firebase AssetBundle Provider")]
     public class FirebaseStorageAssetBundleProvider : AssetBundleProvider
     {
+        public readonly Dictionary<string, AsyncOperationHandle<IAssetBundleResource>> bundleOperationHandles = new Dictionary<string, AsyncOperationHandle<IAssetBundleResource>>();
+        
+        public override void Release(IResourceLocation location, object asset)
+        {
+            base.Release(location, asset);
+
+            // We have to make sure that the actual Bundle Load operation for this asset also gets released together with the Firebase Resource
+            if (bundleOperationHandles.TryGetValue(location.InternalId, out AsyncOperationHandle<IAssetBundleResource> opertion))
+            {
+                if (opertion.IsValid())
+                {
+                    Addressables.ResourceManager.Release(opertion);
+                }
+                bundleOperationHandles.Remove(location.InternalId);
+            }
+        }
+
         public override void Provide(ProvideHandle provideHandle)
         {
             if (FirebaseAddressablesManager.IsFirebaseSetupFinished)
@@ -64,8 +83,19 @@ namespace RobinBird.FirebaseTools.Storage.Addressables
                     Data = provideHandle.Location.Data,
                     PrimaryKey = provideHandle.Location.PrimaryKey
                 };
-                
-                provideHandle.ResourceManager.ProvideResource<IAssetBundleResource>(bundleLoc).Completed += handle =>
+
+                AsyncOperationHandle<IAssetBundleResource> asyncOperationHandle;
+                if (bundleOperationHandles.TryGetValue(provideHandle.Location.InternalId, out asyncOperationHandle))
+                {
+                    // Release already running handler
+                    if (asyncOperationHandle.IsValid())
+                    {
+                        provideHandle.ResourceManager.Release(asyncOperationHandle);
+                    }
+                }
+                asyncOperationHandle = provideHandle.ResourceManager.ProvideResource<IAssetBundleResource>(bundleLoc);
+                bundleOperationHandles.Add(provideHandle.Location.InternalId, asyncOperationHandle);
+                asyncOperationHandle.Completed += handle =>
                 {
                     provideHandle.Complete(handle.Result, true, null);
                 };
